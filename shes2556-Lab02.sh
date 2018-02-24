1e82c169c9188a70235369fc90720b927963f5df
eyad <shes2556@stthomas.edu>
2018-02-24 07:07:31 +0000

    2nd script commit

--- shes2556-Lab02.sh
+++ shes2556-Lab02.sh
@@ -9,7 +9,7 @@
 
 
 mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
-touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll}}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
 sudo apt-get update
 apt-get -y install tree
 apt-get -y install git
