10a042ed7d506edecee323502f8c2d28b84df6be
eyad <shes2556@stthomas.edu>
2018-02-24 07:03:32 +0000

    script commit

--- shes2556-Lab02.sh
+++ shes2556-Lab02.sh
@@ -8,8 +8,8 @@
 
 
 
-mkdir -p vagrant/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
-touch vagrant/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll}}}
+mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll}}}
 sudo apt-get update
 apt-get -y install tree
 apt-get -y install git
